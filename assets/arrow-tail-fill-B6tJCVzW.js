1f5651bf12a35badd35490d7f813b9cbda236674
Nishanth Ilango <nishanth.ilango@syncfusion.com>
2026-02-04 12:47:05 +0530

    Modified the UI

---
+++ assets/arrow-tail-fill-B6tJCVzW.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-7s7xaQU8.js";import{i as l}from"./month-view-Cf91h4wJ.js";const e=o.jsx("path",{d:"M10 5.99997L2 12L10 18V13H22V11H10V5.99997Z",fillRule:"evenodd",clipRule:"evenodd",fill:"currentcolor"}),n=l(e),r=o.jsx("path",{d:"M14 5.99997L22 12L14 18V13H2V11H14V5.99997Z",fillRule:"evenodd",clipRule:"evenodd",fill:"currentcolor"}),i=l(r);export{n as A,i as a};
